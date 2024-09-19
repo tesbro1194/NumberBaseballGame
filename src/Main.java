b7a1500e27702e9465d31299b2defa330185a0f2
tesbro1194 <tes1194@naver.com>
2024-09-19 12:20:45 +0900

    주석 삭제 테스트

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args)  {
-//        MakeNumber makeNumber = new MakeNumber();
-//        ArrayList<Integer> numberList = new ArrayList<>(makeNumber.makeNumbers());
-        // numberList 를 섞어서 크기대로 나열되는 문제 해결 ?
-        // 애초에 왜 크기대로 나열되는 것일까요.
-//        System.out.println(numberList);
-//        boolean endGameKey = false;
-
-//        do {
-//
-//        } while ();
         System.out.println("* 숫자 야구 게임 *");
 
         Beginning beginning = new Beginning();
